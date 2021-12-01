4abbd4084f60eda8d40da7171c200f1e132bd656
James Hulse <james.hulse@gmail.com>
2021-12-01 14:29:50 +1300

    2020 Day 1 part 2 complete.

--- 2020/day-1/src/main.rs
+++ 2020/day-1/src/main.rs
@@ -5,24 +5,34 @@ fn main() {
     println!("Hello, world!");
 
     let lines = fs::read_to_string("input.txt").expect("Couldn't read input file.");
-    let mut combos = lines
+    let values = lines
         .split("\n")
-        .map(|val| val.parse::<u32>().expect("Nope!"))
-        .combinations(2);
+        .map(|val| val.parse::<u32>().expect("Invalid value in input file!"))
+        .collect();
 
-    let result: Result<Vec<u32>, &str> = loop {
-        match combos.next() {
-            Some(val) if val[0] + val[1] == 2020 => break Ok(val),
-            Some(_) => continue,
-            None => break Err("Couldn't find matching pair"),
-        };
-    };
+    let result = find_matching_combinations(values, 3);
 
     match result {
         Ok(val) => {
-            println!("Found the combintation: {:?}", val);
-            println!("The result is: {:?}", val[0] * val[1]);
+            println!("Found the combination: {:?}", val);
+            println!("The result is: {:?}", val.iter().product::<u32>());
         }
         Err(e) => println!("{}", e),
+    }
+}
+
+// TODO: Could I make 'input' an iterable so we don't have to map the whole file?
+fn find_matching_combinations(
+    input: Vec<u32>,
+    combo_size: usize,
+) -> Result<Vec<u32>, &'static str> {
+    let mut combos = input.into_iter().combinations(combo_size);
+
+    loop {
+        match combos.next() {
+            Some(val) if val.iter().sum::<u32>() == 2020 => break Ok(val),
+            Some(_) => continue,
+            None => break Err("Couldn't find matching pair"),
+        };
     }
 }
