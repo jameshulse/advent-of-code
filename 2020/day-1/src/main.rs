b86a6bd831033d8752866215770c585b74578145
James Hulse <james.hulse@gmail.com>
2021-12-01 23:55:16 +1300

    2020 day 1 fix ups

--- 2020/day-1/src/main.rs
+++ 2020/day-1/src/main.rs
@@ -6,33 +6,48 @@ fn main() {
 
     let lines = fs::read_to_string("input.txt").expect("Couldn't read input file.");
     let values: Vec<u32> = lines
-        .split("\n")
+        .lines()
         .map(|val| val.parse::<u32>().expect("Invalid value in input file!"))
         .collect();
 
-    let result = find_matching_combinations(values, 3);
+    let result = find_matching_combinations(&values, 3);
 
     match result {
-        Ok(val) => {
+        Some(val) => {
             println!("Found the combination: {:?}", val);
             println!("The result is: {:?}", val.iter().product::<u32>());
         }
-        Err(e) => println!("{}", e),
+        None => println!("Couldn't find matching combination"),
     }
 }
 
 // TODO: Could I make 'input' an iterable so we don't have to map the whole file?
-fn find_matching_combinations(
-    input: Vec<u32>,
-    combo_size: usize,
-) -> Result<Vec<u32>, &'static str> {
-    let mut combos = input.into_iter().combinations(combo_size);
+fn find_matching_combinations(input: &Vec<u32>, combo_size: usize) -> Option<Vec<u32>> {
+    return input
+        .iter()
+        .cloned() // TODO: Can I get rid of this? IT will use a lot more memory
+        .combinations(combo_size)
+        .find(|x| x.iter().cloned().sum::<u32>() == 2020)
+        .clone();
+}
 
-    loop {
-        match combos.next() {
-            Some(val) if val.iter().sum::<u32>() == 2020 => break Ok(val),
-            Some(_) => continue,
-            None => break Err("Couldn't find matching pair"),
-        };
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_find_two_combos() {
+        let values = vec![1721, 979, 366, 299, 675, 1456];
+        let result = find_matching_combinations(&values, 2);
+
+        assert_eq!(result.unwrap(), vec![1721, 299])
+    }
+
+    #[test]
+    fn test_find_three_combos() {
+        let values = vec![1721, 979, 366, 299, 675, 1456];
+        let result = find_matching_combinations(&values, 3);
+
+        assert_eq!(result.unwrap(), vec![979, 366, 675]);
     }
 }
