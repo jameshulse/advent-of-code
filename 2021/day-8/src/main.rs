8eea053de17588231c004a841320dcefc8725803
James Hulse <james.hulse@gmail.com>
2021-12-09 19:16:37 +1300

    2021 day 8 complete

--- 2021/day-8/src/main.rs
+++ 2021/day-8/src/main.rs
@@ -1,5 +1,5 @@
 use itertools::Itertools;
-use std::{borrow::Borrow, collections::HashMap};
+use std::collections::HashMap;
 
 fn main() {
     let entries = &parse_input(include_str!("input"));
@@ -89,17 +89,26 @@ fn parse_input(input: &str) -> Vec<Entry> {
 // cefabd cdfgeb cagedb -> (remove 'f' and 'g') -> ceabd cdeb caedb -> "c" solved as "a"
 
 // Solve for F
-// Remove "c" from 1 digits
-fn solve_mappings(entry: &Entry) -> HashMap<&str, String> {
-    let mut mappings: HashMap<&str, String> = HashMap::new();
+// Remove "c" from 1 digits -> "f" is solved as "b"
 
+// Solution:
+// A -> d
+// B -> e
+// C -> a
+// D -> f
+// E -> g
+// F -> b
+// G -> c
+fn solve_mappings(entry: &Entry) -> HashMap<String, String> {
     let one = find_by_len(&entry.input, 2);
     let four = find_by_len(&entry.input, 4);
     let seven = find_by_len(&entry.input, 3);
-    let eight = find_by_len(&entry.input, 7);
 
     // Solve for "a"
-    mappings.insert("a", seven.replace(one, ""));
+    let solved_a = seven
+        .chars()
+        .filter(|&ch| !one.contains(ch))
+        .collect::<String>();
 
     // Solve for "b" and "e"
     // - Find the two characters which only occur once (representing 'b' and 'e' in digits 2 and 5)
@@ -117,29 +126,31 @@ fn solve_mappings(entry: &Entry) -> HashMap<&str, String> {
         })
         .collect_vec();
 
+    let mut solved_b = String::new();
+    let mut solved_e = String::new();
+
     for ch in single_chars {
         if four.contains(ch) {
             // If the character matches a character in digit 4 then it is the "b"
-            mappings.insert("b", ch.to_string());
+            solved_b = ch.to_string();
         } else {
             // If it doesn't match a character in digit 4 then it must be the "e"
-            mappings.insert("e", ch.to_string());
+            solved_e = ch.to_string();
         }
     }
 
     // Solve for "d"
     let solved_d = four
-        .replace(one, "")
-        .replace(mappings.get("b").unwrap(), "");
-
-    mappings.insert("d", solved_d);
+        .chars()
+        .filter(|&ch| !one.contains(ch) && !solved_b.contains(ch))
+        .collect::<String>();
 
     // Solve for "g" - Take 5 len digits, remove 'a', 'd' and find triple occurence
     let triple_chars = len_five
         .iter()
         .flat_map(|s| {
-            s.replace(mappings.get("a").unwrap(), "")
-                .replace(mappings.get("d").unwrap(), "")
+            s.replace(&solved_a, "")
+                .replace(&solved_d, "")
                 .chars()
                 .collect_vec()
         })
@@ -152,7 +163,9 @@ fn solve_mappings(entry: &Entry) -> HashMap<&str, String> {
         })
         .collect_vec();
 
-    mappings.insert("g", triple_chars[0].to_string());
+    let solved_g = triple_chars[0].to_string();
+
+    // mappings.insert(triple_chars[0].to_string(), "g".to_string());
 
     // Solve for "c" -> Take 6 digit, remove 'd' and find letter occurring twice
     let len_six = entry.input.iter().filter(|e| e.len() == 6).collect_vec();
@@ -160,8 +173,8 @@ fn solve_mappings(entry: &Entry) -> HashMap<&str, String> {
     let double_chars = len_six
         .iter()
         .flat_map(|s| {
-            s.replace(mappings.get("e").unwrap(), "")
-                .replace(mappings.get("d").unwrap(), "")
+            s.replace(&solved_e, "")
+                .replace(&solved_d, "")
                 .chars()
                 .collect_vec()
         })
@@ -174,45 +187,54 @@ fn solve_mappings(entry: &Entry) -> HashMap<&str, String> {
         })
         .collect_vec();
 
-    mappings.insert("c", double_chars[0].to_string());
+    let solved_c = double_chars[0].to_string();
+
+    // mappings.insert(double_chars[0].to_string(), "c".to_string());
 
     // Solve for "f" -> Remove 'c' from 1
-    let solved_f = one.replace(mappings.get("c").unwrap(), "");
+    let solved_f = one.replace(&solved_c, "");
 
-    mappings.insert("f", solved_f);
+    let mut mappings: HashMap<String, String> = HashMap::new();
+
+    mappings.insert(solved_a.to_string(), "a".to_string());
+    mappings.insert(solved_b.to_string(), "b".to_string());
+    mappings.insert(solved_c.to_string(), "c".to_string());
+    mappings.insert(solved_d.to_string(), "d".to_string());
+    mappings.insert(solved_e.to_string(), "e".to_string());
+    mappings.insert(solved_f.to_string(), "f".to_string());
+    mappings.insert(solved_g.to_string(), "g".to_string());
 
     mappings
 }
 
-const ZERO: String = "abcefg".to_string();
-const TWO: String = "acdeg".to_string();
-const THREE: String = "acdfg".to_string();
-const FIVE: String = "abdfg".to_string();
-const SIX: String = "abdefg".to_string();
-const NINE: String = "abcdfg".to_string();
-
-fn resolve_output(mappings: &HashMap<&str, String>, output: &Vec<String>) -> usize {
+// bcdef -[map]> fgabd -[sort]> abdfg -[match]> "5"
+fn resolve_output(mappings: &HashMap<String, String>, output: &Vec<String>) -> usize {
     let mut out_str: String = "".to_string();
 
     for unmapped in output {
-        let result = match unmapped.len() {
-            2 => '1',
-            4 => '4',
-            3 => '7',
-            7 => '8',
-            _ => match unmapped
-                .chars()
-                .map(|c| mappings.get(&c.to_string()).unwrap())
-                .join("")
-            {
-                ZERO => '0',
-                TWO => '2',
-                THREE => '2',
-                _ => panic!("Mapping not found!"),
+        let mapped = unmapped
+            .chars()
+            .map(|c| mappings.get(&c.to_string()).unwrap())
+            .sorted()
+            .join("");
+
+        let result: Option<&str> = match mapped.len() {
+            2 => Some("1"),
+            4 => Some("4"),
+            3 => Some("7"),
+            7 => Some("8"),
+            _ => match mapped.as_str() {
+                "abcefg" => Some("0"),
+                "acdeg" => Some("2"),
+                "acdfg" => Some("3"),
+                "abdfg" => Some("5"),
+                "abdefg" => Some("6"),
+                "abcdfg" => Some("9"),
+                _ => None,
             },
         };
 
-        out_str.push(result);
+        out_str += result.unwrap();
     }
 
     out_str.parse().unwrap()
@@ -267,13 +289,13 @@ fn test_solve_entry() {
 
     let mappings = solve_mappings(&entries[0]);
 
-    assert_eq!(mappings.get("a").unwrap(), &"d".to_string());
-    assert_eq!(mappings.get("b").unwrap(), &"e".to_string());
-    assert_eq!(mappings.get("c").unwrap(), &"a".to_string());
-    assert_eq!(mappings.get("d").unwrap(), &"f".to_string());
-    assert_eq!(mappings.get("e").unwrap(), &"g".to_string());
-    assert_eq!(mappings.get("f").unwrap(), &"b".to_string());
-    assert_eq!(mappings.get("g").unwrap(), &"c".to_string());
+    assert_eq!(mappings.get("d").unwrap(), &"a".to_string());
+    assert_eq!(mappings.get("e").unwrap(), &"b".to_string());
+    assert_eq!(mappings.get("a").unwrap(), &"c".to_string());
+    assert_eq!(mappings.get("f").unwrap(), &"d".to_string());
+    assert_eq!(mappings.get("g").unwrap(), &"e".to_string());
+    assert_eq!(mappings.get("b").unwrap(), &"f".to_string());
+    assert_eq!(mappings.get("c").unwrap(), &"g".to_string());
 }
 
 #[test]
@@ -283,18 +305,23 @@ fn test_resolve_output() {
     );
     let mut mappings = HashMap::new();
 
-    mappings.insert("a", "d".to_string());
-    mappings.insert("b", "e".to_string());
-    mappings.insert("c", "a".to_string());
-    mappings.insert("d", "f".to_string());
-    mappings.insert("e", "g".to_string());
-    mappings.insert("f", "b".to_string());
-    mappings.insert("g", "c".to_string());
+    mappings.insert("d".to_string(), "a".to_string());
+    mappings.insert("e".to_string(), "b".to_string());
+    mappings.insert("a".to_string(), "c".to_string());
+    mappings.insert("f".to_string(), "d".to_string());
+    mappings.insert("g".to_string(), "e".to_string());
+    mappings.insert("b".to_string(), "f".to_string());
+    mappings.insert("c".to_string(), "g".to_string());
 
-    assert_eq!(resolve_output(&mappings, &entries[0].output), 5355);
+    assert_eq!(resolve_output(&mappings, &entries[0].output), 5353);
 }
 
-// #[test]
-// fn test_digit_to_binary() {
-//     assert_eq!(digit_to_binary(&"cf".to_owned()), 0b0010010);
-// }
+#[test]
+fn test_example_1() {
+    let entries = parse_input(
+        indoc! {"be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe"},
+    );
+    let mappings = solve_mappings(&entries[0]);
+
+    assert_eq!(resolve_output(&mappings, &entries[0].output), 8394);
+}
