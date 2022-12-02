58d62b8e6ad4bed3ae817f4aac6862bec97b2563
James Hulse <james.hulse@gmail.com>
2022-12-02 19:56:44 +1300

    2022 day 2 complete

--- 2022/day-2/lib/day2.ex
+++ 2022/day-2/lib/day2.ex
@@ -1,24 +1,78 @@
 defmodule Day2 do
-  @moduledoc """
-  Documentation for `Day2`.
-  """
+  # First = opponent
+  # A = Rock
+  # B = Paper
+  # C = Scissors
 
-  @doc """
-  Hello world.
+  ### Part 1
 
-  ## Examples
+  # Second = response
+  # X = Rock = 1
+  # Y = Paper = 2
+  # Z = Scissors = 3
 
-      iex> Day2.hello()
-      :world
-
-  """
+  # Loss = 0
+  # Draw = 3
+  # Win = 6
   def part1 do
     File.read!("input")
-    # |> String.split("\n", trim: true)
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line -> String.split(line, " ", trim: true) end)
+    |> Enum.map(fn [first, second] -> calculate_score(first, second) end)
+    |> Enum.sum()
   end
 
+  def calculate_score(opponent, response) do
+    type_score =
+      case response do
+        "X" -> 1
+        "Y" -> 2
+        "Z" -> 3
+      end
+
+    outcome_score =
+      case {opponent, response} do
+        {"A", "X"} -> 3
+        {"A", "Y"} -> 6
+        {"A", "Z"} -> 0
+        {"B", "X"} -> 0
+        {"B", "Y"} -> 3
+        {"B", "Z"} -> 6
+        {"C", "X"} -> 6
+        {"C", "Y"} -> 0
+        {"C", "Z"} -> 3
+      end
+
+    type_score + outcome_score
+  end
+
+  ### Part 2
+
+  # X = lose
+  # Y = draw
+  # Z = win
   def part2 do
     File.read!("input")
-    # |> String.split("\n", trim: true)
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line -> String.split(line, " ", trim: true) end)
+    |> Enum.map(fn [first, second] -> follow_guide(first, second) end)
+    |> Enum.sum()
+  end
+
+  def follow_guide(opponent, outcome) do
+    response =
+      case {opponent, outcome} do
+        {"A", "X"} -> "Z"
+        {"A", "Y"} -> "X"
+        {"A", "Z"} -> "Y"
+        {"B", "X"} -> "X"
+        {"B", "Y"} -> "Y"
+        {"B", "Z"} -> "Z"
+        {"C", "X"} -> "Y"
+        {"C", "Y"} -> "Z"
+        {"C", "Z"} -> "X"
+      end
+
+    calculate_score(opponent, response)
   end
 end
--- 2022/day-2/test/day2_test.exs
+++ 2022/day-2/test/day2_test.exs
@@ -2,8 +2,24 @@ defmodule Day2Test do
   use ExUnit.Case
   doctest Day2
 
+  test "calculate score" do
+    assert Day2.calculate_score("A", "Y") == 8
+    assert Day2.calculate_score("B", "X") == 1
+    assert Day2.calculate_score("C", "Z") == 6
+  end
+
+  test "follow outcomes" do
+    assert Day2.follow_guide("A", "Y") == 4
+    assert Day2.follow_guide("B", "X") == 1
+    assert Day2.follow_guide("C", "Z") == 7
+  end
+
   test "run parts" do
     IO.puts("Part 1: #{Day2.part1()}")
     IO.puts("Part 2: #{Day2.part2()}")
+  end
+
+  test "part 1" do
+    assert Day2.part1() == 11666
   end
 end
