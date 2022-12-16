305a5348abc4ddd8c1d5aa795bef864f0f7bf147
James Hulse <james.hulse@gmail.com>
2022-12-16 23:18:05 +1300

    2022 day 15 part 2 complete - but had to hack answer out of output

--- 2022/day-15/lib/day15.ex
+++ 2022/day-15/lib/day15.ex
@@ -1,28 +1,42 @@
 defmodule Day15 do
   def part1(input, intersection_row) do
-    input
-    |> read_sensor_list()
-    |> Enum.map(fn %{sensor: {sx, sy} = sensor, beacon: {bx, by}} ->
-      radius = abs(sx - bx) + abs(sy - by)
+    %{sensors: sensors, beacons: beacons} = read_sensor_list(input)
 
-      intersection(sensor, radius, intersection_row)
+    sensors
+    |> Enum.map(fn {sx, sy, radius} ->
+      intersection({sx, sy}, radius, intersection_row)
     end)
     |> Enum.filter(fn intersection -> intersection != :none end)
-    |> IO.inspect()
+    |> Enum.sort()
     |> then(&reduce_ranges/1)
-    |> IO.inspect()
     |> Enum.map(fn range ->
-      size = Range.size(range)
-      
-      size
+      width = Range.size(range)
+
+      beacons_inline =
+        Enum.count(beacons, fn {bx, by} -> bx in range and by == intersection_row end)
+
+      width - beacons_inline
     end)
     |> Enum.sum()
+  end
+
+  def overlap?(r1, r2) do
+    cond do
+      r1.first < r2.first ->
+        r1.last + 1 >= r2.first
+
+      r2.first < r1.first ->
+        r2.last + 1 >= r1.first
+
+      true ->
+        true
+    end
   end
 
   def reduce_ranges(ranges) do
     overlap =
       Enum.zip(ranges, Enum.drop(ranges, 1))
-      |> Enum.find(fn {l, r} -> !Range.disjoint?(l, r) end)
+      |> Enum.find(fn {l, r} -> overlap?(l, r) end)
 
     case overlap do
       {l, r} ->
@@ -36,9 +50,51 @@ defmodule Day15 do
     end
   end
 
-  # def part2(input) do
+  def part2(input, search_range) do
+    %{sensors: sensors, beacons: beacons} = read_sensor_list(input)
 
-  # end
+    search_range
+    |> Stream.map(fn row ->
+      coverage =
+        sensors
+        |> Enum.map(fn {sx, sy, radius} -> intersection({sx, sy}, radius, row) end)
+        |> Enum.filter(fn intersection -> intersection != :none end)
+        |> Enum.map(fn intersection ->
+          # Constrain intersection to search range
+          max(search_range.first, intersection.first)..min(intersection.last, search_range.last)
+        end)
+        # Add beacons so that we don't count them as empty space
+        |> Enum.concat(
+          beacons
+          |> Enum.flat_map(fn {bx, by} ->
+            if by == row and bx in search_range do
+              [bx..bx]
+            else
+              []
+            end
+          end)
+        )
+        |> Enum.sort()
+        |> then(&reduce_ranges/1)
+
+      {row, coverage}
+    end)
+    |> Stream.filter(fn {_row, coverage} -> length(coverage) > 1 end)
+    |> Stream.take(1)
+    |> Enum.to_list()
+    |> List.first()
+    |> IO.inspect()
+    |> then(fn {row, [r1, r2]} ->
+      col =
+        cond do
+          r1.first < r2.first -> r1.last + 1
+          r2.first < r1.first -> r2.last + 1
+        end
+
+      col * 4_000_000 + row
+    end)
+    |> IO.inspect()
+  end
 
   def read_sensor_list(input) do
     input
@@ -49,21 +105,16 @@ defmodule Day15 do
           ~r/^Sensor at x=(?<sx>-?\d+), y=(?<sy>-?\d+): closest beacon is at x=(?<bx>-?\d+), y=(?<by>-?\d+)$/,
           line
         )
-        
+
       sx = String.to_integer(sx)
       sy = String.to_integer(sy)
       bx = String.to_integer(bx)
       by = String.to_integer(by)
       radius = abs(sx - bx) + abs(sy - by)
-      
-      %{
-        sensors: sensors ++ [{sx, sy, radius}],
-        beacons: beacons ++ [{bx, by}]
-      }
 
       %{
-        sensor: {, String.to_integer(sy)},
-        beacon: {String.to_integer(bx), String.to_integer(by)}
+        sensors: sensors ++ [{sx, sy, radius}],
+        beacons: Enum.uniq(beacons ++ [{bx, by}])
       }
     end)
   end
@@ -83,5 +134,3 @@ defmodule Day15 do
     end
   end
 end
-
-..####B######################..
--- 2022/day-15/test/day15_test.exs
+++ 2022/day-15/test/day15_test.exs
@@ -18,14 +18,19 @@ defmodule Day15Test do
   Sensor at x=20, y=1: closest beacon is at x=15, y=3
   """
 
-  # test "both parts" do
-  #   input = File.read!("input")
+  test "both parts" do
+    input = File.read!("input")
 
-  #   assert Day15.part1(input, 2000000) == -1
-  # end
+    assert Day15.part1(input, 2_000_000) == 4_951_427
+    assert Day15.part2(input, 0..4_000_000) == -1
+  end
 
   test "part1" do
     assert Day15.part1(@test_input, 10) == 26
+  end
+
+  test "part2" do
+    assert Day15.part2(@test_input, 0..20) == 56_000_011
   end
 
   test "intersection" do
@@ -38,5 +43,21 @@ defmodule Day15Test do
 
   test "reduce_ranges" do
     assert Day15.reduce_ranges([12..12, 2..14, 2..2, -2..2, 16..24, 14..18]) == [-2..24]
+    assert Day15.reduce_ranges([0..6, 7..20]) == [0..20]
+
+    assert Day15.reduce_ranges([1_178_096..2_044_526, 0..1_248_064]) == [0..2_044_526]
+
+    assert Day15.reduce_ranges(
+             [3_666_940..4_000_000, 2_658_353..3_257_427, 3_257_429..3_859_523, 0..2_780_821]
+             |> Enum.sort()
+           ) == [0..4_000_000]
+  end
+
+  test "overlap?" do
+    assert Day15.overlap?(0..5, 6..10) == true
+    assert Day15.overlap?(0..5, 2..8) == true
+    assert Day15.overlap?(3..4, 5..5) == true
+    assert Day15.overlap?(6..9, 3..4) == false
+    assert Day15.overlap?(3..4, 6..9) == false
   end
 end
