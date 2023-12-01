a335c7e2ea42d060b45b70ffcea1fc6b010e1803
James Hulse <james.hulse@gmail.com>
2023-12-01 22:10:19 +1300

    2023 day 1 refactor part 2
    2023 day 1 refactor part 2

--- 2023/Day1.fsx
+++ 2023/Day1.fsx
@@ -17,14 +17,12 @@ let input = getInput 2023 1
 
 let part1 (data: string) =
     data.Split("\n", StringSplitOptions.RemoveEmptyEntries)
-    |> Seq.map (fun (line) -> Regex.Replace(line, "[abcdefghijklmnopqrstuvwxyz]", ""))
-    |> Seq.map (fun (digits) -> $"%c{digits[0]}%c{digits[digits.Length - 1]}")
-    |> Seq.map int
+    |> Seq.map (fun (line) -> Regex.Replace(line, @"[^\d]", ""))
+    |> Seq.map (fun (digits) -> int $"%c{digits[0]}%c{digits[digits.Length - 1]}")
     |> Seq.sum
 
 part1 sample // 142
 part1 input // 55123
-
 
 let sample2 =
     """
@@ -37,55 +35,32 @@ zoneight234
 7pqrstsixteen
 """
 
-let numberWords =
-    [ "one", 1
-      "two", 2
-      "three", 3
-      "four", 4
-      "five", 5
-      "six", 6
-      "seven", 7
-      "eight", 8
-      "nine", 9 ]
-    |> dict
+let matchToNumber (input: Match) =
+    match input.Value with
+    | "one" -> 1
+    | "two" -> 2
+    | "three" -> 3
+    | "four" -> 4
+    | "five" -> 5
+    | "six" -> 6
+    | "seven" -> 7
+    | "eight" -> 8
+    | "nine" -> 9
+    | v -> int v
 
-let findNumAt (input: string) index =
-    seq {
-        for i in 1..9 do
-            yield (i |> string, i)
+let getCalibration (input: string) =
+    let pattern = @"\d|one|two|three|four|five|six|seven|eight|nine"
+    let first = Regex(pattern).Match(input)
 
-        for KeyValue (name, value) in numberWords do
-            yield (name, value)
-    }
-    |> Seq.choose (fun ((name, value)) ->
-        let test = input.[index .. index + name.Length - 1]
+    let last =
+        Regex(pattern, RegexOptions.RightToLeft)
+            .Match(input)
 
-        // printfn $"({name} {value}) %s{test}"
+    (matchToNumber first) * 10 + (matchToNumber last)
 
-        if test = name then
-            Some(value)
-        else
-            None)
-    |> Seq.tryExactlyOne
-
-let findNums (input: string) =
-    [ for i in 0 .. input.Length do
-          findNumAt input i ]
-    |> Seq.choose id
-    |> Seq.toArray
-
-findNumAt "one2" 0
-findNumAt "one2" 3
-
-findNums "one2"
-findNums "eighttwothree"
-findNums "xtwoone3four"
-
-let part2 (data: string) =
+let part2Take2 (data: string) =
     data.Split("\n", StringSplitOptions.RemoveEmptyEntries)
-    |> Seq.map findNums
-    |> Seq.map (fun (digits) -> digits[0] * 10 + digits[digits.Length - 1])
-    |> Seq.sum
+    |> Seq.sumBy getCalibration
 
-part2 sample2 // 281
-part2 input // 55260
+part2Take2 sample2 // 281
+part2Take2 input // 55260
