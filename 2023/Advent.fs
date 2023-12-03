1fef1d2137109994b93854685a72d7a9d3d75bce
James Hulse <james.hulse@gmail.com>
2023-12-04 11:22:00 +1300

    2023 day 3 final refactor

--- 2023/Advent.fs
+++ 2023/Advent.fs
@@ -6,6 +6,7 @@ open System.Diagnostics
 open FsHttp
 open FsHttp.Response
 
+(* Helper methods *)
 let getInput year day =
     let session = File.ReadAllText ".session"
     let url = $"https://adventofcode.com/%d{year}/day/%d{day}/input"
@@ -28,3 +29,18 @@ let bench func =
     let result = func ()
     sw.Stop()
     (result, sw.Elapsed)
+
+(* Extension methods *)
+module Array =
+    (* Like mapi, it passes the index to each iteration being collected *)
+    let collecti collector arr =
+        arr
+        |> Array.mapi (fun i next -> (i, next))
+        |> Array.collect (fun (i, next) -> collector i next)
+
+module Seq =
+    (* Like mapi, it passes the index to each iteration being collected *)
+    let collecti collector arr =
+        arr
+        |> Seq.mapi (fun i next -> (i, next))
+        |> Seq.collect (fun (i, next) -> collector i next)
--- 2023/Day3.fsx
+++ 2023/Day3.fsx
@@ -20,7 +20,7 @@ let sample =
 
 let input = getInput 2023 3
 
-type Part =
+type PartNumber =
     { Value: int
       Y: int
       XStart: int
@@ -28,41 +28,34 @@ type Part =
 
 let matchNumbers = Regex(@"\d{1,3}", RegexOptions.Compiled)
 
-let findNumbersOnLine line =
-    matchNumbers.Matches(line)
-    |> Seq.map (fun number -> (int number.Value, number.Index, number.Length))
-    |> Seq.toArray
-
-let findParts lines =
+let findPartNumbers lines =
     lines
-    |> Array.mapi (fun y line -> (y, findNumbersOnLine line))
-    |> Array.collect (fun (y, numbers) ->
-        numbers
-        |> Array.map (fun (value, ind, len) ->
-            { Value = value
+    |> Array.collecti (fun y line ->
+        matchNumbers.Matches(line)
+        |> Seq.toArray
+        |> Array.map (fun number ->
+            { Value = int number.Value
               Y = y
-              XStart = ind
-              XEnd = ind + len - 1 }))
+              XStart = number.Index
+              XEnd = number.Index + number.Length - 1 }))
 
-type Symbol = { Value: string; X: int; Y: int }
+type Symbol = { Glyph: char; X: int; Y: int }
 
 let matchSymbols = Regex(@"[^\d\.]{1}", RegexOptions.Compiled)
 
-let findSymbolsOnLine line =
-    matchSymbols.Matches(line)
-    |> Seq.map (fun symbol -> (symbol.Value, symbol.Index))
-    |> Seq.toArray
-
 let findSymbols lines =
     lines
-    |> Array.mapi (fun y line -> (y, findSymbolsOnLine line))
-    |> Array.collect (fun (y, symbols) ->
-        symbols
-        |> Array.map (fun (symbol, x) -> { Value = symbol; X = x; Y = y }))
+    |> Seq.collecti (fun y line ->
+        matchSymbols.Matches(line)
+        |> Seq.map (fun symbol ->
+            { Glyph = char symbol.Value
+              X = symbol.Index
+              Y = y }))
+    |> Seq.toArray
 
-let findAdjacentParts allParts symbol =
-    allParts
-    |> Array.filter (fun (part: Part) ->
+let findAdjacentPartNumbers allPartNumbers symbol =
+    allPartNumbers
+    |> Array.filter (fun (part: PartNumber) ->
         symbol.Y >= part.Y - 1
         && symbol.Y <= part.Y + 1
         && symbol.X >= part.XStart - 1
@@ -71,28 +64,24 @@ let findAdjacentParts allParts symbol =
 let part1 data =
     let lines = splitByLine data
     let symbols = findSymbols lines
-    let parts = findParts lines
+    let adjacentPartFinder = findPartNumbers lines |> findAdjacentPartNumbers
 
     symbols
-    |> Array.collect (fun symbol -> findAdjacentParts parts symbol)
-    |> Array.sumBy (fun part -> part.Value)
+    |> Seq.collect (fun symbol -> adjacentPartFinder symbol)
+    |> Seq.sumBy (fun part -> part.Value)
 
 part1 sample // 4361
 part1 input // 554003
 
 let part2 data =
     let lines = splitByLine data
+    let parts = findPartNumbers lines
 
-    let gears =
-        findSymbols lines
-        |> Array.filter (fun s -> s.Value = "*")
-
-    let parts = findParts lines
-
-    gears
-    |> Array.map (findAdjacentParts parts)
-    |> Array.filter (fun adjacent -> adjacent.Length = 2)
-    |> Array.sumBy (fun adjacent -> adjacent[0].Value * adjacent[1].Value)
+    findSymbols lines
+    |> Seq.filter (fun symbol -> symbol.Glyph = '*')
+    |> Seq.map (findAdjacentPartNumbers parts)
+    |> Seq.filter (fun adjacent -> adjacent.Length = 2)
+    |> Seq.sumBy (fun adjacent -> adjacent[0].Value * adjacent[1].Value)
 
 part2 sample // 467835
 part2 input // 87263515
