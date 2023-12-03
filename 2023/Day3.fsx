8926e50591b746ad6e006ce8ad4a63ea1c5449f0
James Hulse <james.hulse@gmail.com>
2023-12-03 20:39:03 +1300

    2023 day 3 part 2 complete

--- 2023/Day3.fsx
+++ 2023/Day3.fsx
@@ -27,7 +27,7 @@ let matchSymbols = Regex(@"[^\d\.]", RegexOptions.Compiled)
 
 let findSymbols line =
     matchSymbols.Matches(line)
-    |> Seq.map (fun symbol -> symbol.Index)
+    |> Seq.map (fun symbol -> (symbol.Value, symbol.Index))
     |> Seq.toArray
 
 
@@ -66,7 +66,7 @@ let part1 data =
     let symbols =
         lines
         |> Array.mapi (fun y line -> (y, findSymbols line))
-        |> Array.collect (fun (y, symbols) -> symbols |> Array.map (fun x -> (x, y)))
+        |> Array.collect (fun (y, symbols) -> symbols |> Array.map (fun (_, x) -> (x, y)))
 
     let parts =
         lines
@@ -86,7 +86,43 @@ let part1 data =
 part1 sample
 part1 input
 
-let part2 data = ()
+let adjacentParts parts symbol =
+    let (symX, symY) = symbol
+
+    parts
+    |> Array.filter (fun (part: Part) ->
+        symY >= part.Y - 1
+        && symY <= part.Y + 1
+        && symX >= part.XStart - 1
+        && symX <= part.XEnd + 1)
+
+let part2 data =
+    let lines = splitByLine data
+
+    let asterisks =
+        lines
+        |> Array.mapi (fun y line -> (y, findSymbols line))
+        |> Array.collect (fun (y, symbols) ->
+            symbols
+            |> Array.map (fun (symbol, x) -> (symbol, (x, y))))
+        |> Array.filter (fun (symbol, _pos) -> symbol = "*")
+        |> Array.map (fun (_symbol, pos) -> pos)
+
+    let parts =
+        lines
+        |> Array.mapi (fun y line -> (y, findNumbers line))
+        |> Array.collect (fun (y, numbers) ->
+            numbers
+            |> Array.map (fun (value, ind, len) ->
+                { Value = value
+                  Y = y
+                  XStart = ind
+                  XEnd = ind + len - 1 }))
+
+    asterisks
+    |> Array.map (adjacentParts parts)
+    |> Array.filter (fun parts -> parts.Length = 2)
+    |> Array.sumBy (fun parts -> parts[0].Value * parts[1].Value)
 
 part2 sample
 part2 input
--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 Documenting my attempts at the Advent of Code competition over the years. I use the event generally as an opportunity
 to try a new language.
 
+    2023 - F#
     2022 - Elixir
     2021 - Rust
     2020 - Ruby
