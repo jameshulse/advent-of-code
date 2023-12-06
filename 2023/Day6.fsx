5abe2a7148ffb58824ddbd56acf7d4a463aa828a
James Hulse <james.hulse@gmail.com>
2023-12-07 09:33:33 +1300

    2023 day 6 part 2 complete

--- 2023/Day6.fsx
+++ 2023/Day6.fsx
@@ -14,6 +14,9 @@ Time:      7  15   30
 Distance:  9  40  200
 """
 // hold dist
+// x = h * (t - h)
+// h^2 - ht + x = 0
+
 // 0 -> 0
 // 1 -> 6
 // 2 -> 10 (2 * (7 - 2))
@@ -26,22 +29,26 @@ let parseLine line =
     line
     |> splitSpaces
     |> Array.skip 1
-    |> Array.map int
+    |> Array.map uint64
 
-type Race = { Time: int; Distance: int }
+type Race = { Time: uint64; Distance: uint64 }
 
 let waysToWin (race: Race) =
     seq {
-        for hold in 1 .. (race.Time) do
-            (hold, hold * (race.Time - hold))
+        let mutable hold = 1UL
+
+        while true do
+            yield hold
+            hold <- hold + 1UL
     }
+    |> Seq.map (fun (hold) -> (hold, hold * (race.Time - hold)))
     |> Seq.skipWhile (fun (_, dist) -> dist <= race.Distance)
-    |> Seq.takeWhile (fun (_, dist) -> dist > race.Distance)
+    |> Seq.takeWhile (fun (hold, dist) -> dist > race.Distance && hold < race.Time)
     |> Seq.length
 
-waysToWin { Time = 7; Distance = 9 }
-waysToWin { Time = 15; Distance = 40 }
-waysToWin { Time = 30; Distance = 200 }
+waysToWin { Time = 7UL; Distance = 9UL } // 4
+waysToWin { Time = 15UL; Distance = 40UL } // 8
+waysToWin { Time = 30UL; Distance = 200UL } // 9
 
 let part1 data =
     let lines = splitByLine data
@@ -57,14 +64,16 @@ let part1 data =
     |> Array.map waysToWin
     |> Array.fold (fun acc curr -> acc * curr) 1
 
-part1 sample // 288?
-part1 input
+part1 sample // 288
+part1 input // 1155175
 
-waysToWin { Time = 71530; Distance = 940200 }
+waysToWin { Time = 71530UL; Distance = 940200UL } // 71503
 
-let part2 data =
-    // waysToWin { Time = 60808676u; Distance = 601116315591300u }
-    0
+let part2 =
+    let time = 60_80_86_76UL
+    let distance = 601_1163_1559_1300UL
 
-part2 sample
-part2 input
+    (* Brute force! *)
+    waysToWin { Time = time; Distance = distance }
+
+part2 // 35961505
